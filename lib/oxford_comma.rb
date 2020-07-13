11e3299edd82fd200d754f9d72361e1fac348bf4
Hiren Mistry <hirenmst@gmail.com>
2020-07-13 02:19:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,7 @@
 def oxford_comma(array)
   if array.length == 1 
     array.join
-  elsif array.length == 2 
+  else array.length == 2 
     array.join("and ")
-  elsif array.length > 2 
-    array.join
+  end
 end
